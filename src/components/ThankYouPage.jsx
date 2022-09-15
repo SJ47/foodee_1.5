5e28b241aa8281bb9d9e928916747649b8a8f5af
Scott Johnston <scott@umachan.co.uk>
2022-09-15 21:07:27 +0100

    Fix table errors and add v2 logo

--- src/components/ThankYouPage.jsx
+++ src/components/ThankYouPage.jsx
@@ -1,31 +1,33 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import '../css/ThankYouPage.css';
-import '../css/animations.css';
-import WashingMachine from '../media/PlateYellow.png'
-
+import "../css/ThankYouPage.css";
+import "../css/animations.css";
+import WashingMachine from "../media/PlateYellow.png";
 
 const ThankYouPage = () => {
-
     return (
         <>
             <div className="thank-you-page">
-
-                <h1 className="large-text-thanks">Thank you for your order.<br></br><br></br>We're cooking it now.</h1>
+                <h1 className="large-text-thanks">
+                    Thank you for your order.<br></br>
+                    <br></br>We're cooking it now.
+                </h1>
                 <div id="washing-machine-container">
-                    <img id="washing-machine" src={WashingMachine} alt="washing machine" />
+                    <img
+                        id="washing-machine"
+                        src={WashingMachine}
+                        alt="washing machine"
+                    />
                 </div>
 
                 <Link to="/home">
-                    <button className="home-btn" type="submit">Home</button>
+                    <button className="home-btn" type="submit">
+                        Home
+                    </button>
                 </Link>
-
-
-
             </div>
-
         </>
-    )
-}
+    );
+};
 
 export default ThankYouPage;
