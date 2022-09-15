65e18750829946ccf7f2f03414f7e4edb74fdc56
Scott Johnston <scott@umachan.co.uk>
2022-09-15 11:13:47 +0100

    Add data files and dataloader

---
+++ data/dataloader.js
@@ -0,0 +1,38 @@
+// Load the AWS SDK for JS
+const fs = require("fs");
+
+// -----------------------------------------
+// Create the document client interface for DynamoDB
+const ddbDocumentClient = require("./config")
+
+console.log("Loading song data into DynamoDB");
+
+const menuItemData = JSON.parse(fs.readFileSync('menu_items.json', 'utf8'));
+menuItemData.forEach(function (menuItem) {
+    const params = {
+        TableName: "foodee-menu_items",
+        Item: {
+            "id": menuItem.id,
+            "category": menuItem.category,
+            "name": menuItem.name,
+            "description": menuItem.description,
+            "price": menuItem.price,
+            "image": menuItem.image,
+            "glutenFree": menuItem.glutenFree,
+            "vegetarian": menuItem.vegetarian,
+            "vegan": menuItem.vegan,
+            "alcoholic": menuItem.alcoholic,
+            "size": menuItem.size,
+            "abv": menuItem.abv,
+            "allergins": menuItem.allergins,
+        }
+    };
+
+    ddbDocumentClient.put(params, function (err, data) {
+        if (err) {
+            console.error("Error adding item!!");
+        } else {
+            console.log("Successfully added menu item: ", menuItem.name);
+        }
+    });
+});
